e5d73e89576d6b534286b23b6ec43be7079efa6f
Xretic <95174481+unf6@users.noreply.github.com>
2026-01-31 14:36:30 +0300

    Initialize main.go with command execution logic

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+        "github.com/unf6/nucleus/cmd"
+        "github.com/charmbracelet/log"
+)
+
+func main() {
+        if err := cmd.Execute(); err != nil {
+                log.Error("Command Execution Failed: ", err)
+                os.Exit(1)
+        }
+} 
