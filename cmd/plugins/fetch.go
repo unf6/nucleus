e33a6262a6a331a56c531b3e88d54c2547b2ffc6
Xretic <95174481+unf6@users.noreply.github.com>
2026-02-01 10:51:09 +0300

    Implement fetch command for plugin metadata retrieval
    This file implements the fetch command for plugins, allowing users to fetch metadata for all plugins, specific plugins, and display them in different formats.

---
+++ cmd/plugins/fetch.go
@@ -0,0 +1,126 @@
+package plugins
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+  "github.com/unf6/nucleus/internal/config"
+	core "github.com/unf6/nucleus/internal/plugins"
+
+	"github.com/spf13/cobra"
+)
+
+var fetchCmd = &cobra.Command{
+	Use:   "fetch",
+	Short: "Fetch plugin metadata",
+}
+
+var fetchAllCmd = &cobra.Command{
+	Use: "all",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := core.UpdateAllRepos(); err != nil {
+			return err
+		}
+
+		for repo := range config.Repos {
+			base := filepath.Join(core.CacheBase, repo)
+			entries, _ := os.ReadDir(base)
+
+			for _, e := range entries {
+				dir := filepath.Join(base, e.Name())
+				if !core.ValidatePluginDir(dir) {
+					continue
+				}
+
+				m, err := core.LoadManifest(dir + "/manifest.json")
+				if err != nil {
+					continue
+				}
+
+				img := m.Img
+				if img == "" {
+					img = "none"
+				}
+
+				fmt.Printf(
+					"id: %s\nname: %s\nversion: %s\nauthor: %s\ndescription: %s\nimg: %s\nrepo: %s\n---\n",
+					m.ID, m.Name, m.Version, m.Author,
+					m.Description, img, repo,
+				)
+			}
+		}
+		return nil
+	},
+}
+
+var fetchMachineCmd = &cobra.Command{
+	Use: "all-machine",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := core.UpdateAllRepos(); err != nil {
+			return err
+		}
+
+		for repo := range config.Repos {
+			base := filepath.Join(core.CacheBase, repo)
+			entries, _ := os.ReadDir(base)
+
+			for _, e := range entries {
+				dir := filepath.Join(base, e.Name())
+				if !core.ValidatePluginDir(dir) {
+					continue
+				}
+
+				m, _ := core.LoadManifest(dir + "/manifest.json")
+				img := m.Img
+				if img == "" {
+					img = "none"
+				}
+
+				fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+					m.ID, m.Name, m.Version, m.Author,
+					m.Description, img, repo,
+				)
+			}
+		}
+		return nil
+	},
+}
+
+var fetchOneCmd = &cobra.Command{
+	Use:  "<pluginId>",
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := core.UpdateAllRepos(); err != nil {
+			return err
+		}
+
+		repo, dir, err := core.FindPlugin(args[0])
+		if err != nil {
+			return err
+		}
+
+		m, err := core.LoadManifest(dir + "/manifest.json")
+		if err != nil {
+			return err
+		}
+
+		img := m.Img
+		if img == "" {
+			img = "none"
+		}
+
+		fmt.Printf(
+			"id: %s\nname: %s\nversion: %s\nauthor: %s\ndescription: %s\nimg: %s\nrepo: %s\n---\n",
+			m.ID, m.Name, m.Version, m.Author,
+			m.Description, img, repo,
+		)
+		return nil
+	},
+}
+
+func init() {
+	fetchCmd.AddCommand(fetchAllCmd, fetchMachineCmd)
+	Cmd.AddCommand(fetchCmd)
+	Cmd.AddCommand(fetchOneCmd)
+}
