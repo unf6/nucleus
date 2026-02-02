029a5499e768dcfab6fd2b4661499c270804ef77
Xretic <95174481+unf6@users.noreply.github.com>
2026-02-02 16:44:25 +0300

    Create ipc.go

---
+++ cmd/ipc.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/unf6/nucleus/internal/ipc"
+)
+
+func ipcCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "ipc [target] [function] [args...]",
+		Short: "Interact with the shell via IPC",
+		Long: "Call QuickShell IPC targets and functions dynamically.",
+		Args: cobra.ArbitraryArgs,
+		DisableFlagParsing: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			ipc.RunShellIPCCommand(args)
+		},
+	}
+  
+	cmd.ValidArgsFunction = func(
+		_ *cobra.Command,
+		args []string,
+		toComplete string,
+	) ([]string, cobra.ShellCompDirective) {
+		completions := ipc.GetShellIPCCompletions(args, toComplete)
+		return completions, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	return cmd
+}
+
+func init() {
+	rootCmd.AddCommand(ipcCmd)
+}
