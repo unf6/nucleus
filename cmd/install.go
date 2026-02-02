6329d4e5129631a1629fbf80992d30c85cfb4210
Xretic <95174481+unf6@users.noreply.github.com>
2026-02-02 22:28:34 +0300

    Update install.go

--- cmd/install.go
+++ cmd/install.go
@@ -27,9 +27,9 @@ var installCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(installCmd)
 	installCmd.Flags().BoolP("force", "f", false, "Force reinstall (removes existing installation)")
-	installCmd.Flags().Bool("dev", false, "Clone full git repository (for development)")
-    installCmd.Flags().Bool("stable", false, "Update latest stable release")
-    installCmd.Flags().Bool("indev", false, "Update latest pre-release")
+	installCmd.Flags().Bool("dev", false, "d", "Clone full git repository (for development)")
+    installCmd.Flags().Bool("stable", false, "s", "Update latest stable release")
+    installCmd.Flags().Bool("indev", false, "i", "Update latest pre-release")
 
 }
 
